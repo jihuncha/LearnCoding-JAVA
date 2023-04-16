c80f3756aa68d69e6163fcb1f0bd946cb8369aa5
Jihun <jihun5000@naver.com>
2023-04-16 21:40:19 +0900

    feat: add homework

---
+++ src/variable/FirstVariableTest.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class FirstVariableTest {
+    public static void main(String[] args) {
+        int myIntegerNumber;
+        myIntegerNumber = 5;
+
+        System.out.println(myIntegerNumber);
+    }
+}
